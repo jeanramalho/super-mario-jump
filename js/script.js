df97a328e6523b9980e4a9f15806ae5a08c19656
jeanramalho <jeanramalho.dev@gmail.com>
2022-06-29 22:37:40 -0300

    Implementa funcao de game-over

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
 
 function jump() {
@@ -8,5 +9,27 @@ function jump() {
         mario.classList.remove('jump')
     }, 500)
 }
+
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "")
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './assets/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+        mario.style.zIndex = '1'
+
+        clearInterval(loop)
+    }
+}, 10)
+
 
 document.addEventListener('keydown', jump)
