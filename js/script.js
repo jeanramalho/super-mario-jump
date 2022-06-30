a4437d3595923a1b9a49ef71c465eb9b12b233ea
jeanramalho <jeanramalho.dev@gmail.com>
2022-06-29 21:48:31 -0300

    Implementa pulo do Mario

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const mario = document.querySelector('.mario')
+
+
+function jump() {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500)
+}
+
+document.addEventListener('keydown', jump)
